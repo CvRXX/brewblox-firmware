8903cfb532845b3bc3c555733d5a1e75fc12fb57
Bob Steers <steers.bob@gmail.com>
2021-05-27 12:03:17 +0200

    fix path in build script

--- build/build-sim-native.sh
+++ build/build-sim-native.sh
@@ -4,11 +4,11 @@ pushd "$(dirname "$0")" > /dev/null
 
 if [[ $(arch) != 'x86_64' ]]; then
     # -m64 is not supported for ARM
-    sed -i 's/-m64//g' platform/spark/device-os/build/gcc-tools.mk
+    sed -i 's/-m64//g' ../platform/spark/device-os/build/gcc-tools.mk
 fi
 
 make APP=brewblox PLATFORM=gcc
 
 # reset modified file
-cd platform/spark/device-os
+cd ../platform/spark/device-os
 git checkout -- build/gcc-tools.mk 
