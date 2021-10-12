6d6bc56d4073d669c5e29d2f94cffed984b0ec84
Carlos van Rooijen <info@carlosvanrooijen.nl>
2021-10-12 18:09:11 +0200

    split data en command functions and harmonise naming

--- platform/esp/TFT035.hpp
+++ platform/esp/TFT035.hpp
@@ -87,11 +87,15 @@ private:
     hal_spi::error_t setPos(unsigned int xs, unsigned int xe, unsigned int ys, unsigned int ye);
 
     template <std::size_t n>
-    hal_spi::error_t dmaWrite(const std::array<uint8_t, n>& tx_val, bool dc);
-    hal_spi::error_t dmaWrite(const uint8_t, bool dc);
-    hal_spi::error_t dmaWrite(const uint8_t* tx_data, size_t tx_len, bool dc);
-    hal_spi::error_t writeCmd(const std::vector<uint8_t>& cmd);
-    hal_spi::error_t write(const std::vector<uint8_t>& cmd);
-    hal_spi::error_t writeCmd(uint8_t cmd);
-    hal_spi::error_t write(uint8_t cmd);
+    hal_spi::error_t dmaWriteCommand(const std::array<uint8_t, n>& tx_val);
+    template <std::size_t n>
+    hal_spi::error_t dmaWriteData(const std::array<uint8_t, n>& tx_val);
+    hal_spi::error_t dmaWriteCommand(uint8_t tx_val);
+    hal_spi::error_t dmaWriteData(uint8_t tx_val);
+    hal_spi::error_t dmaWriteCommand(const uint8_t* tx_data, size_t tx_len);
+    hal_spi::error_t dmaWriteData(const uint8_t* tx_data, size_t tx_len);
+    hal_spi::error_t writeCommand(const std::vector<uint8_t>& cmd);
+    hal_spi::error_t writeData(const std::vector<uint8_t>& cmd);
+    hal_spi::error_t writeCommand(uint8_t cmd);
+    hal_spi::error_t writeData(uint8_t cmd);
 };
