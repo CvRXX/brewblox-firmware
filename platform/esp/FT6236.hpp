153ea3f89f44db90c183775cd6c4514108b27ca8
Carlos van Rooijen <info@carlosvanrooijen.nl>
2021-10-20 16:51:33 +0200

    fix various touchscreen bugs & improve readabilty

--- platform/esp/FT6236.hpp
+++ platform/esp/FT6236.hpp
@@ -3,78 +3,69 @@
 #include "driver/gpio.h"
 #include "hal/hal_gpio.h"
 #include <atomic>
-#include <esp_log.h>
 #include <optional>
 
+/**
+ * A driver for the FT6236 touchscreen controller
+ */
 class FT6236 : public I2CDeviceBase<0x38> {
 public:
-    FT6236(uint8_t address)
-        : I2CDeviceBase(address)
-    {
-    }
-    ~FT6236()
-    {
-        // Disable interupts and unregister handler
-        gpio_set_intr_type(int_pin, GPIO_INTR_DISABLE);
-        gpio_isr_handler_remove(int_pin);
-    }
+    /** 
+    * Constructs the driver
+    * 
+    * @param address The lower address of the i2c device
+    */
+    FT6236(uint8_t address);
+    ~FT6236();
 
-    struct touch {
-        uint8_t x;
-        uint8_t y;
+    /// The different gestures the FT6236 supports
+    enum class Gesture : std::uint8_t {
+        moveUp,
+        moveRight,
+        moveDown,
+        moveLeft,
+        zoomIn,
+        zoomOut,
+        noGesture
     };
 
-    void init()
-    {
-        //Enable interupt polling mode
-        writeRegister(0xA4, 0x00);
+    /// A structure to represent a touch event
+    struct Touch {
+        uint16_t x;      // The x position of where the touch happened on the screen
+        uint16_t y;      // The y position of where the touch happened on the screen
+        Gesture gesture; // A possible gesture that occurred during the touch
+    };
 
-        //Enable interupts and register handler
-        gpio_set_intr_type(int_pin, GPIO_INTR_NEGEDGE);
-        gpio_isr_handler_add(
-            int_pin, [](void* data) {
-                *reinterpret_cast<std::atomic<bool>*>(data) = true;
-            },
-            reinterpret_cast<void*>(&interuptFlag));
-    }
+    /// Initialises the driver, needs to be called before other functions
+    void init();
 
-    bool writeRegister(uint8_t reg, uint8_t value)
-    {
-        return i2c_write({reg, value});
-    }
+    /** Returns the lastTouch that occurred. 
+     *  If no touch has happened it will return a touch at {0,0} with no gesture.
+     */
+    Touch getLastTouch();
 
-    std::optional<uint8_t> readRegister(uint8_t reg)
-    {
-        auto value = std::array<uint8_t, 1>{};
-        i2c_write(reg);
-        auto read = i2c_read(value);
-        if (read) {
-            return value[0];
-        } else {
-            return std::nullopt;
-        }
-    }
-    std::optional<touch> getTouch()
-    {
-        if (interuptFlag.exchange(false)) {
-            auto x = readRegister(0x06);
-            auto y = readRegister(0x04);
-            if (x && y) {
-                return touch{*x, *y};
-            } else {
-                return std::nullopt;
-            }
-        } else {
-            return std::nullopt;
-        }
-    }
-    uint8_t getGesture() // Make this with the new read and make it optional.
-    {
-        i2c_write(0x01);
-        return i2c_read(1)[0]; // TODO: handle empty vector
-    }
+    /**
+     * Checks if a touch is happening or happened.
+     * @returns If a touch happened the touch otherwise std::nullopt
+     */
+    std::optional<Touch> getTouch();
 
 private:
+    enum class Register : uint8_t {
+        P1_XH = 0x03,
+        P1_XL = 0x04,
+        P1_YH = 0x05,
+        P1_YL = 0x06,
+        GEST_ID = 0x01,
+        G_MODE = 0xA4
+    };
+
+    bool writeRegister(Register reg, uint8_t value);
+    std::optional<uint8_t> readRegister(Register reg);
+
+    Gesture getGesture();
+
+    Touch lastTouch = Touch{0, 0, Gesture::noGesture};
     const gpio_num_t int_pin = GPIO_NUM_35;
     std::atomic_bool interuptFlag = false;
 };
