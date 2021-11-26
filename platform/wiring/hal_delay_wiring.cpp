1b1859cbb9c48dd9b09aa9829debbe13a8313b85
Elco Jacobs <elco@brewpi.com>
2021-11-26 17:02:10 +0100

    fix yield for simulator

--- platform/wiring/hal_delay_wiring.cpp
+++ platform/wiring/hal_delay_wiring.cpp
@@ -1,5 +1,12 @@
-#include "concurrent_hal.h"
 #include "delay_hal.h"
+#include "platforms.h"
+
+#if PLATFORM_ID == PLATFORM_GCC
+#include "boost_posix_time_wrap.h"
+#include "boost_thread_wrap.h"
+#else
+#include "concurrent_hal.h"
+#endif
 
 void hal_delay_us(uint32_t duration)
 {
@@ -13,5 +20,10 @@ void hal_delay_ms(uint32_t duration)
 
 void hal_yield()
 {
+
+#if PLATFORM_ID == PLATFORM_GCC
+    boost::this_thread::yield();
+#else
     os_thread_yield();
+#endif
 }
