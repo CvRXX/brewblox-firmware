2a678123c334be0342a16f3778b262be129bf66b
Carlos van Rooijen <info@carlosvanrooijen.nl>
2022-05-16 17:02:28 +0200

    add error type for decoding

--- lib/guis/dynamic_gui/decode.hpp
+++ lib/guis/dynamic_gui/decode.hpp
@@ -7,11 +7,22 @@
 #include "elements/widgets/numeric_value_widget.hpp"
 #include "elements/widgets/widget.hpp"
 #include "proto/ScreenConfig.pb.h"
+#include "tl/expected.hpp"
 #include <iostream>
 #include <optional>
 #include <pb_decode.h>
 
 namespace gui::dynamic_interface {
+
+enum class DecodeError : uint8_t {
+    success = 0,
+    bufferIsNullptr = 1,
+    unknownLayoutType = 2,
+    PBError = 3,
+    nodeIdsAreNotSequentialPerLevel = 4,
+
+};
+
 namespace detail {
 
     // Decode layoutNodes and push them to a vector passed in arg.
@@ -44,7 +55,7 @@ namespace detail {
         return true;
     }
 
-    bool parseRowCol(blox_ScreenConfig_LayoutNode& nodeToAdd, std::vector<std::pair<std::unique_ptr<Element>, uint32_t>>& elements)
+    DecodeError parseRowCol(blox_ScreenConfig_LayoutNode& nodeToAdd, std::vector<std::pair<std::unique_ptr<Element>, uint32_t>>& elements)
     {
         auto children = std::vector<std::unique_ptr<Element>>{};
 
@@ -69,10 +80,10 @@ namespace detail {
             std::unique_ptr<Element> newElement{new VerticalSplit(std::move(children), nodeToAdd.weight, nodeToAdd.nodeId)};
             elements.push_back({std::move(newElement), nodeToAdd.parent});
         }
-        return true;
+        return DecodeError::success;
     }
 
-    bool parseContent(blox_ScreenConfig_LayoutNode& nodeToAdd, std::vector<std::pair<std::unique_ptr<Element>, uint32_t>>& elements, std::vector<blox_ScreenConfig_ContentNode>& contentNodes)
+    DecodeError parseContent(blox_ScreenConfig_LayoutNode& nodeToAdd, std::vector<std::pair<std::unique_ptr<Element>, uint32_t>>& elements, std::vector<blox_ScreenConfig_ContentNode>& contentNodes)
     {
         // Find the content node with a layoutId that matches the id of the current node.
         auto content = std::find_if(contentNodes.begin(), contentNodes.end(), [nodeToAdd](const auto& node) {
@@ -105,9 +116,9 @@ namespace detail {
             std::unique_ptr<Element> newElement{new Content(nodeToAdd.weight, nodeToAdd.nodeId, std::move(widget))};
             elements.push_back({std::move(newElement), nodeToAdd.parent});
         }
-        return true;
+        return DecodeError::success;
     }
-    std::optional<Screen> decodeNodes(std::vector<blox_ScreenConfig_LayoutNode>& layoutNodes, std::vector<blox_ScreenConfig_ContentNode> contentNodes)
+    tl::expected<Screen, DecodeError> decodeNodes(std::vector<blox_ScreenConfig_LayoutNode>& layoutNodes, std::vector<blox_ScreenConfig_ContentNode> contentNodes)
     {
         // Temp storage of elements which are to be children of higher elements.
         auto elements = std::vector<std::pair<std::unique_ptr<Element>, uint32_t>>{};
@@ -122,29 +133,35 @@ namespace detail {
         // This means that the children of a node are already created when the node is reached.
         for (auto& nodeToAdd : layoutNodes) {
             if (nodeToAdd.type == blox_ScreenConfig_Type_Row || nodeToAdd.type == blox_ScreenConfig_Type_Column) {
-                if (!parseRowCol(nodeToAdd, elements)) {
-                    return std::nullopt;
+                auto status = parseRowCol(nodeToAdd, elements);
+                if (status != DecodeError::success) {
+                    return tl::unexpected{status};
                 }
             } else if (nodeToAdd.type == blox_ScreenConfig_Type_Content) {
-                if (!parseContent(nodeToAdd, elements, contentNodes)) {
-                    return std::nullopt;
+                auto status = parseContent(nodeToAdd, elements, contentNodes);
+                if (status != DecodeError::success) {
+                    return tl::unexpected{status};
                 }
             } else {
-                return std::nullopt;
+                return tl::unexpected{DecodeError::unknownLayoutType};
             }
         }
 
         // The only element left should have a parent of id: 0.
         if (elements.size() != 1 || elements.back().second != 0) {
-            return std::nullopt;
+            return tl::unexpected{DecodeError::nodeIdsAreNotSequentialPerLevel};
         }
 
         return Screen{std::move(elements.back().first)};
     }
 }
 
-std::optional<Screen> decodeBuffer(uint8_t* buffer, size_t length)
+tl::expected<Screen, DecodeError> decodeBuffer(uint8_t* buffer, size_t length)
 {
+    if (!buffer) {
+        return tl::unexpected{DecodeError::bufferIsNullptr};
+    }
+
     blox_ScreenConfig_ScreenConfig protoMessage = blox_ScreenConfig_ScreenConfig_init_default;
 
     protoMessage.layoutNodes.funcs.decode = detail::layoutNodeDecoder;
@@ -157,7 +174,7 @@ std::optional<Screen> decodeBuffer(uint8_t* buffer, size_t length)
 
     pb_istream_t stream = pb_istream_from_buffer(buffer, length);
     if (!pb_decode(&stream, blox_ScreenConfig_ScreenConfig_fields, &protoMessage)) {
-        return std::nullopt;
+        return tl::unexpected{DecodeError::PBError};
     }
 
     return detail::decodeNodes(layoutNodes, contentNodes);
