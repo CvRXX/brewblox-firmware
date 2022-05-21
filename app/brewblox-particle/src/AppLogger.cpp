98daf57431b473cbabfe31da0b236ad6b3680d4a
Elco Jacobs <elco@brewpi.com>
2022-05-21 10:58:28 +0200

    make logger consistent across platforms

--- app/brewblox-particle/src/AppLogger.cpp
+++ app/brewblox-particle/src/AppLogger.cpp
@@ -6,13 +6,26 @@ namespace logger {
 
 void _write(char category, const std::string& message)
 {
-    std::string combined;
-    combined.reserve(message.size() + 3);
-    combined.push_back(category);
-    combined.push_back(':');
-    combined.push_back(' ');
+    auto combined = std::string("LOG(I): ");
+    combined[4] = category;
     combined.append(message);
+
     platform::particle::getConnectionPool().writeLog(combined);
+}
+
+void info(const std::string& message)
+{
+    _write('I', message);
+}
+
+void warn(const std::string& message)
+{
+    _write('W', message);
+}
+
+void error(const std::string& message)
+{
+    _write('E', message);
 }
 
 } // end namespace logger
--- test/shared/AppLogger.hpp
+++ test/shared/AppLogger.hpp
@@ -5,44 +5,16 @@
 
 namespace logger {
 
-extern std::vector<std::string> logs;
+void clear();
 
-inline void clear()
-{
-    logs.clear();
-}
+bool contains(const std::string& s);
 
-inline bool contains(const std::string& s)
-{
-    return std::any_of(std::begin(logs), std::end(logs), [&s](const std::string& entry) {
-        return entry == s;
-    });
-}
+uint32_t count(const std::string& s);
 
-inline uint32_t count(const std::string& s)
-{
-    uint32_t count = 0;
-    for (const auto& log : logs) {
-        if (s == log) {
-            count++;
-        }
-    }
-    return count;
-}
+void info(const std::string& message);
 
-inline void info(const std::string& message)
-{
-    logs.push_back("LOG(I): " + message);
-}
+void warn(const std::string& message);
 
-inline void warn(const std::string& message)
-{
-    logs.push_back("LOG(W): " + message);
-}
-
-inline void error(const std::string& message)
-{
-    logs.push_back("LOG(E): " + message);
-}
+void error(const std::string& message);
 
 } // end namespace logger
