03d0ade073dbe735ecdc3998f28c95edd0e861a1
Bob Steers <steers.bob@gmail.com>
2022-07-18 14:28:45 +0200

    Add ipv4 encoding to edge case

--- lib/proto/inc/proto/EdgeCase.pb.h
+++ lib/proto/inc/proto/EdgeCase.pb.h
@@ -48,6 +48,7 @@ typedef struct _blox_EdgeCase_Block {
     uint32_t logged;
     uint32_t unLogged;
     uint16_t drivenDevice;
+    uint32_t ip;
 /* @@protoc_insertion_point(struct:blox_EdgeCase_Block) */
 } blox_EdgeCase_Block;
 
@@ -57,12 +58,12 @@ typedef struct _blox_EdgeCase_Block {
 #define blox_EdgeCase_Settings_init_default      {0, 0}
 #define blox_EdgeCase_State_init_default         {0, 0}
 #define blox_EdgeCase_NestedLink_init_default    {0}
-#define blox_EdgeCase_Block_init_default         {blox_EdgeCase_Settings_init_default, blox_EdgeCase_State_init_default, 0, {{NULL}, NULL}, {{NULL}, NULL}, 0, 0, 0, 0}
+#define blox_EdgeCase_Block_init_default         {blox_EdgeCase_Settings_init_default, blox_EdgeCase_State_init_default, 0, {{NULL}, NULL}, {{NULL}, NULL}, 0, 0, 0, 0, 0}
 #define blox_EdgeCase_SubCase_init_default       {0}
 #define blox_EdgeCase_Settings_init_zero         {0, 0}
 #define blox_EdgeCase_State_init_zero            {0, 0}
 #define blox_EdgeCase_NestedLink_init_zero       {0}
-#define blox_EdgeCase_Block_init_zero            {blox_EdgeCase_Settings_init_zero, blox_EdgeCase_State_init_zero, 0, {{NULL}, NULL}, {{NULL}, NULL}, 0, 0, 0, 0}
+#define blox_EdgeCase_Block_init_zero            {blox_EdgeCase_Settings_init_zero, blox_EdgeCase_State_init_zero, 0, {{NULL}, NULL}, {{NULL}, NULL}, 0, 0, 0, 0, 0}
 #define blox_EdgeCase_SubCase_init_zero          {0}
 
 /* Field tags (for use in manual encoding/decoding) */
@@ -81,12 +82,13 @@ typedef struct _blox_EdgeCase_Block {
 #define blox_EdgeCase_Block_logged_tag           7
 #define blox_EdgeCase_Block_unLogged_tag         8
 #define blox_EdgeCase_Block_drivenDevice_tag     9
+#define blox_EdgeCase_Block_ip_tag               10
 
 /* Struct field encoding specification for nanopb */
 extern const pb_field_t blox_EdgeCase_Settings_fields[3];
 extern const pb_field_t blox_EdgeCase_State_fields[3];
 extern const pb_field_t blox_EdgeCase_NestedLink_fields[2];
-extern const pb_field_t blox_EdgeCase_Block_fields[10];
+extern const pb_field_t blox_EdgeCase_Block_fields[11];
 extern const pb_field_t blox_EdgeCase_SubCase_fields[2];
 
 /* Maximum encoded size of messages (where known) */
